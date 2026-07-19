68ff19d2064ea617168f4acd335f93602bb2081b
SaffronWare <aryanshadzi@gmail.com>
2026-07-18 22:36:42 -0400

    ROTATIONSSS

--- src/Mat4.hpp
+++ src/Mat4.hpp
@@ -17,13 +17,55 @@ namespace ark
 		static Mat4 identity()
 		{
 			Mat4 mat;
-			mat.rows[0] = _mm_set_ps(0.0f, 0.0f, 0.0f, 1.0f);
-			mat.rows[1] = _mm_set_ps(0.0f, 0.0f, 1.0f, 0.0f);
-			mat.rows[2] = _mm_set_ps(0.0f, 1.0f, 0.0f, 0.0f);
-			mat.rows[3] = _mm_set_ps(1.0f, 0.0f, 0.0f, 0.0f);
+			mat.rows[0] = _mm_setr_ps(0.0f, 0.0f, 0.0f, 1.0f);
+			mat.rows[1] = _mm_setr_ps(0.0f, 0.0f, 1.0f, 0.0f);
+			mat.rows[2] = _mm_setr_ps(0.0f, 1.0f, 0.0f, 0.0f);
+			mat.rows[3] = _mm_setr_ps(1.0f, 0.0f, 0.0f, 0.0f);
 			return mat;
 		}
 
+		static Mat4 rotateX(float angle)
+		{
+			float c = std::cosf(angle);
+			float s = std::sinf(angle);
+
+			Mat4 mat;
+			mat.rows[0] = _mm_setr_ps(1.0f, 0.0f, 0.0f, 0.0f);
+			mat.rows[1] = _mm_setr_ps(0.0f, c, -s, 0.0f);
+			mat.rows[2] = _mm_setr_ps(0.0f, s, c, 0.0f);
+			mat.rows[3] = _mm_setr_ps(0.0f, 0.0f, 0.0f, 1.0f);
+
+			return mat;
+		}
+
+		static Mat4 rotateY(float angle)
+		{
+			float c = std::cosf(angle);
+			float s = std::sinf(angle);
+
+			Mat4 mat;
+			mat.rows[0] = _mm_setr_ps(c, 0.0f, s, 0.0f);
+			mat.rows[1] = _mm_setr_ps(0.0f, 1.0f, 0.0f, 0.0f);
+			mat.rows[2] = _mm_setr_ps(-s, 0.0f, c, 0.0f);
+			mat.rows[3] = _mm_setr_ps(0.0f, 0.0f, 0.0f, 1.0f);
+
+			return mat;
+		}
+
+		static Mat4 rotateZ(float angle)
+		{
+			float c = std::cosf(angle);
+			float s = std::sinf(angle);
+
+			Mat4 mat;
+			mat.rows[0] = _mm_setr_ps(c, -s, 0.0f, 0.0f);
+			mat.rows[1] = _mm_setr_ps(s, c, 0.0f, 0.0f);
+			mat.rows[2] = _mm_setr_ps(0.0f, 0.0f, 1.0f, 0.0f);
+			mat.rows[3] = _mm_setr_ps(0.0f, 0.0f, 0.0f, 1.0f);
+
+			return mat;
+		}
+		
 	};
 
 	
